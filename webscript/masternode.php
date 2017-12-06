13914112f07fabb976edddcf2384abdafb62842f
sfairchild <sfairchild@trinet-hi.com>
2017-12-06 10:31:47 -1000

    update

--- webscript/masternode.php
+++ webscript/masternode.php
@@ -1,5 +1,13 @@
 <?php 
 
-echo $_POST["masternodeprivkey"]; 
+$masternodeprivkey = $_POST["masternodeprivkey"]; 
+$file = '/var/www/masternodeprivkey/masternodeprivkey.txt';
+file_put_contents($file, $masternodeprivkey);
+
+echo "masternodeprivkey recieved. pepecoinmasternode docker container will be starting shortly. If you entered the wrong key, please rebuild";
+
+chmod ("/var/www/html/index.html", 000);
+chmod ("/var/www/html/masternode.php", 000);
 
 ?>
+
