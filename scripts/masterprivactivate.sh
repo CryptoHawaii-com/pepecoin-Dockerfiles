cc78a3ed41dfb13381830161606f834b94c3d348
sfairchild <sfairchild@trinet-hi.com>
2017-12-07 08:08:26 -1000

    update

--- scripts/masterprivactivate.sh
+++ scripts/masterprivactivate.sh
@@ -14,7 +14,7 @@ while inotifywait -e modify /var/www/masternodeprivkey/masternodeprivkey.txt; do
   echo "addnode=seed2.freepepe.org" >> /root/pepecoin.conf
   echo "addnode=seed3.freepepe.org" >> /root/pepecoin.conf
   echo "addnode=162.248.93.192:29377" >> /root/pepecoin.conf
-  echo "maxconnections=300" >> /root/pepecoin.conf
+  echo "maxconnections=16" >> /root/pepecoin.conf
   echo "masternodeprivkey=$MASTERNODEPRIVKEY" >> /root/pepecoin.conf
   echo "masternode=1" >> /root/pepecoin.conf
   echo "masternopeaddr=$IP:29387" >> /root/pepecoin.conf
